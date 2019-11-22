1c81bc7a1fedb40b2c6a277deb25914abd804272
Dav <57032623+Dav-Git@users.noreply.github.com>
2019-11-22 01:38:40 +0100

    Add files via upload

---
+++ startupdetector/__init__.py
@@ -0,0 +1,4 @@
+from .mycog import Mycog
+
+def setup(bot):
+    bot.add_cog(Mycog())
---
+++ startupdetector/startupdetector.py
@@ -0,0 +1,10 @@
+from redbot.core import commands
+
+class Mycog(commands.Cog):
+    """My custom cog"""
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
