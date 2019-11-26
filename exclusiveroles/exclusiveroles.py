759a405395d570f7b6d3f186412a63b2cde916ab
Dav <57032623+Dav-Git@users.noreply.github.com>
2019-11-26 23:14:21 +0100

    Update exclusiveroles.py
    Added await where i forgot it

--- exclusiveroles/exclusiveroles.py
+++ exclusiveroles/exclusiveroles.py
@@ -19,9 +19,9 @@ class ExclusiveRoles(commands.Cog):
     @commands.command()
     @checks.admin()
     async def retroscan(self, ctx):
-        ctx.send("Retroscan started...")
+        await ctx.send("Retroscan started...")
         for user in ctx.guild.members:
             if ctx.guild.get_role(489455280266936321) in user.roles:
                 if ctx.guild.get_role(634692203582717990) in user.roles:
                     await user.remove_roles(after.guild.get_role(634692203582717990), reason="Active Applied (RETROSCAN)")
-        ctx.send("Retroscan completed.")
+        await ctx.send("Retroscan completed.")
