b10b84bd47eb64c532b87b2fc9d4c9ccf1a11f0d
Dav <57032623+Dav-Git@users.noreply.github.com>
2019-11-26 21:44:52 +0100

    Update exclusiveroles.py

--- exclusiveroles/exclusiveroles.py
+++ exclusiveroles/exclusiveroles.py
@@ -13,5 +13,4 @@ class ExclusiveRoles(commands.Cog):
         role = after.guild.get_role(489455280266936321)
         if not role in before.roles:
             if role in after.roles:
-                await after.remove_roles(after.guild.get_roles(634692203582717990),reason="Active Applied")
-        return
+                await after.remove_roles(after.guild.get_roles(634692203582717990),reason = "Active Applied")
