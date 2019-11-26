46acf65b72e2f9aa4f9b8b536e2300da34045ad5
Dav <57032623+Dav-Git@users.noreply.github.com>
2019-11-26 23:09:54 +0100

    Update exclusiveroles.py

--- exclusiveroles/exclusiveroles.py
+++ exclusiveroles/exclusiveroles.py
@@ -1,5 +1,5 @@
 import discord
-from redbot.core import commands
+from redbot.core import commands, checks
 
 
 class ExclusiveRoles(commands.Cog):
@@ -14,3 +14,14 @@ class ExclusiveRoles(commands.Cog):
         if role not in before.roles:
             if role in after.roles:
                 await after.remove_roles(after.guild.get_role(634692203582717990), reason="Active Applied")
+
+                
+    @commands.command()
+    @checks.admin()
+    async def retroscan(self, ctx):
+        ctx.send("Retroscan started...")
+        for user in ctx.guild.members:
+            if ctx.guild.get_role(489455280266936321) in user.roles:
+                if ctx.guild.get_role(634692203582717990) in user.roles:
+                    await user.remove_roles(after.guild.get_role(634692203582717990), reason="Active Applied (RETROSCAN)")
+        ctx.send("Retroscan completed.")
