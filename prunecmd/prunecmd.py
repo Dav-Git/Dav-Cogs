22e8422305b603da17c56598988dec5ecf8bc623
Dav <57032623+Dav-Git@users.noreply.github.com>
2019-12-18 00:15:37 +0100

    Update prunecmd.py

--- prunecmd/prunecmd.py
+++ prunecmd/prunecmd.py
@@ -2,5 +2,11 @@ from redbot.core import commands, checks
 
 class Prunecmd(commands.Cog):
   """Prunecmd"""
+  def __init__(self):
+    
   
-  
+  @commands.command()
+  @checks.admin()
+  async def prunecmd(self, ctx):
+    await ctx.guild.prune_members(days=1, compute_prune_count=False, reason="Member prune")
+    await ctx.send("The member list has been pruned successfully")
