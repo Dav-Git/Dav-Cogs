44c7622b7afd8b2046bea37c76bfc3830db32a61
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-02-03 03:30:27 +0100

    black formatting

--- bday/bday.py
+++ bday/bday.py
@@ -3,10 +3,9 @@ from discord.ext import tasks
 from redbot.core import commands, checks, Config
 
 
-
 class Bday(commands.Cog):
     """Bday"""
-    
+
     def __init__(self):
         self.config = Config.get_conf(self, identifier=1072001)
         default_guild = {"bdayRole": 0}
@@ -19,11 +18,16 @@ class Bday(commands.Cog):
         """Assigns the birthday role to a member and sends birthday wishes."""
 
         if not await self.config.guild(ctx.guild).bdayRole() == 0:
-            await user.add_roles(ctx.guild.get_role(await self.config.guild(ctx.guild).bdayRole()), reason="It's their birthday!") 
+            await user.add_roles(
+                ctx.guild.get_role(await self.config.guild(ctx.guild).bdayRole()),
+                reason="It's their birthday!",
+            )
             await ctx.send("Happy birthday {} !".format(user.mention))
         else:
-            await ctx.send("You need to configure a birthday role first by using ``[p]setbirthday``.")
-    
+            await ctx.send(
+                "You need to configure a birthday role first by using ``[p]setbirthday``."
+            )
+
     @commands.command()
     @checks.admin()
     async def setbirthday(self, ctx, role: discord.Role):
@@ -32,12 +36,18 @@ class Bday(commands.Cog):
 
         await self.config.guild(ctx.guild).bdayRole.set(role.id)
         await ctx.send("The birthday role has been set to {}".format(role.name))
-    
+
     @commands.command()
     @checks.mod()
     async def clearbirthdays(self, ctx):
 
         """Clears the birthday role off of all members."""
 
-        for user in ctx.guild.get_role(await self.config.guild(ctx.guild).bdayRole()).members:
-            await user.remove_roles(ctx.guild.get_role(await self.config.guild(ctx.guild).bdayRole()), reason="It's a new day. The birthdays are over.")
+        for user in ctx.guild.get_role(
+            await self.config.guild(ctx.guild).bdayRole()
+        ).members:
+            await user.remove_roles(
+                ctx.guild.get_role(await self.config.guild(ctx.guild).bdayRole()),
+                reason="It's a new day. The birthdays are over.",
+            )
+
--- prunecmd/prunecmd.py
+++ prunecmd/prunecmd.py
@@ -1,12 +1,17 @@
 from redbot.core import commands, checks
 
+
 class Prunecmd(commands.Cog):
-  """Prunecmd"""
-  def __init__(self):
-    pass
-  
-  @commands.command()
-  @checks.admin()
-  async def prunecmd(self, ctx):
-    await ctx.guild.prune_members(days=1, compute_prune_count=False, reason="Member prune")
-    await ctx.send("The member list has been pruned successfully")
+    """Prunecmd"""
+
+    def __init__(self):
+        pass
+
+    @commands.command()
+    @checks.admin()
+    async def prunecmd(self, ctx):
+        await ctx.guild.prune_members(
+            days=1, compute_prune_count=False, reason="Member prune"
+        )
+        await ctx.send("The member list has been pruned successfully")
+
