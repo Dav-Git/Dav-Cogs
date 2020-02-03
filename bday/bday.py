71b63b37617f58b1752275ca875b44a4941348dd
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-02-03 03:06:22 +0100

    fixed errors

--- bday/bday.py
+++ bday/bday.py
@@ -13,19 +13,19 @@ class Bday(commands.Cog):
         self.config.register_guild(**default_guild)
 
     @commands.command()
-    @checks.is_mod_or_superior()
+    @checks.mod()
     async def birthday(self, ctx, user: discord.Member):
 
         """Assigns the birthday role to a member and sends birthday wishes."""
 
         if not await self.config.guild(ctx.guild).bdayRole() == 0:
             await user.add_roles(ctx.guild.get_role(await self.config.guild(ctx.guild).bdayRole()), reason="It's their birthday!") 
-            await ctx.send("Happy birthday {} !".format(user.mention()))
+            await ctx.send("Happy birthday {} !".format(user.mention))
         else:
             await ctx.send("You need to configure a birthday role first by using ``[p]setbirthday``.")
     
     @commands.command()
-    @checks.is_admin_or_superior()
+    @checks.admin()
     async def setbirthday(self, ctx, role: discord.Role):
 
         """Set the role that will be assigned on a birthday."""
@@ -34,7 +34,7 @@ class Bday(commands.Cog):
         await ctx.send("The birthday role has been set to {}".format(role.name))
     
     @commands.command()
-    @checks.is_mod_or_superior()
+    @checks.mod()
     async def clearbirthdays(self, ctx):
 
         """Clears the birthday role off of all members."""
