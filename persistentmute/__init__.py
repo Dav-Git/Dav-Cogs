9a1f7885896b520a07eaeb1ebb3f1c5ebf2a44b7
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-02-04 00:02:06 +0100

    turns out persistentmute is fucking hard to make. Will reattempt after core mute rework

--- persistentmute/__init__.py
+++
@@ -1,4 +0,0 @@
-from .persistentmute import PersistentMute
-
-def setup(bot):
-    bot.add_cog(PersistentMute())
--- persistentmute/persistentmute.py
+++
@@ -1,7 +0,0 @@
-import discord
-from redbot.core import commands, checks, Config
-
-class PersistentMute(commands.Cog):
-
-    def __init__(self):
-        pass
