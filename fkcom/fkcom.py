2ddb33868324a18e299d2e1c37c6f79204f8dc11
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-02-11 22:39:18 +0100

    formatting tweaks

--- fkcom/fkcom.py
+++ fkcom/fkcom.py
@@ -8,8 +8,8 @@ class FKCom(commands.Cog):
     @checks.mod()
     async def rp(self, ctx, user: Optional[discord.User]):
         await ctx.send(
-            "Hey {}, it seems like you have asked a question about ARP (Aspirant Gaming). We are not affiliated with ARP at all and sadly can not provide any information to you. To receive an answer to your question, try {} or the Aspirant Gaming discord server.".format(
-                user.mention if user != None else " ",
+            "Hey{}, it seems like you have asked a question about ARP (Aspirant Gaming). We are not affiliated with ARP at all and sadly can not provide any information to you. To receive an answer to your question, try {} or the Aspirant Gaming discord server.".format(
+                " " + user.mention if user != None else "\u200b",
                 ctx.guild.get_channel(478917077705555970).mention,
             )
         )
