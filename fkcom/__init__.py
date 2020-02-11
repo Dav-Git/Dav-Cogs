49607668de2e2e6167bdfa7b8d43f8ac2f9f9a6e
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-02-11 22:31:08 +0100

    Firekitten server specific commands

---
+++ fkcom/__init__.py
@@ -0,0 +1,5 @@
+from .fkcom import FKCom
+
+
+def setup(bot):
+    bot.add_cog(FKCom())
---
+++ fkcom/fkcom.py
@@ -0,0 +1,16 @@
+from redbot.core import checks, commands
+import discord
+from typing import Optional
+
+
+class FKCom(commands.Cog):
+    @commands.command()
+    @checks.mod()
+    async def rp(self, ctx, user: Optional[discord.User]):
+        await ctx.send(
+            "Hey {}, it seems like you have asked a question about ARP (Aspirant Gaming). We are not affiliated with ARP at all and sadly can not provide any information to you. To receive an answer to your question, try {} or the Aspirant Gaming discord server.".format(
+                user.mention if user != None else " ",
+                ctx.guild.get_channel(478917077705555970).mention,
+            )
+        )
+
