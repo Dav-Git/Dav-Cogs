f84944e1011d03827dff5294fc78112b4d945e36
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-03-05 23:26:51 +0100

    remove troll cog

--- troll/__init__.py
+++
@@ -1,5 +0,0 @@
-from .troll import Troll
-
-
-def setup(bot):
-    bot.add_cog(Troll())
--- troll/troll.py
+++
@@ -1,8 +0,0 @@
-from redbot.core import commands
-
-
-class Troll(commands.Cog):
-    @commands.command()
-    async def troll(self, ctx, a: int):
-        for i in range(a):
-            await ctx.send("LULULULULULULULULULULULULULULULULULULULULULULULULULULU{}".format(i))
