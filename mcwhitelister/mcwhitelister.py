dc470584f240757037fba72dc4696fb733e61e37
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-03-12 00:54:54 +0100

    testing

--- mcwhitelister/mcwhitelister.py
+++ mcwhitelister/mcwhitelister.py
@@ -15,6 +15,7 @@ class McWhitelister(commands.Cog):
     async def on_member_remove(self, member):
         p_in_conf = await self.config.guild(member.guild).players()
         if str(member.id) in p_in_conf:
+            print("Bazinga")
             path = await self.config.guild(member.guild).path_to_server()
             with open("{}whitelist.json".format(path)) as json_file:
                 file = json.load(json_file)
