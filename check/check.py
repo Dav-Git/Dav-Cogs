0d3ebf1c44f4200753ad574011d9ef86d4eae282
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-04-20 18:03:59 +0200

    stop passing things

--- check/check.py
+++ check/check.py
@@ -15,18 +15,21 @@ class Check(commands.Cog):
         try:
             member = ctx.guild.get_member(user.id)
             await ctx.send(f":mag_right: Starting lookup for: {user.mention}({user.id})")
+            member = True
         except AttributeError:
             await ctx.send(
                 f":mag: This user is not in your guild anymore. Fetching reduced information for UID: {user.id}."
             )
         try:
-            await ctx.invoke(ctx.bot.get_command("userinfo"), user=member)
+            if member:
+                await ctx.invoke(ctx.bot.get_command("userinfo"), user=member)
         except:
             pass
         try:
             await ctx.invoke(ctx.bot.get_command("read"), user=user)
         except:
-            await ctx.invoke(ctx.bot.get_command("warnings"), user=member)
+            if member:
+                await ctx.invoke(ctx.bot.get_command("warnings"), user=member)
         try:
             await ctx.invoke(ctx.bot.get_command("listflag"), member=user)
         except:
