e58968c47952d4df82fb63e970635d46834457e5
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-04-20 18:11:40 +0200

    jack improvements

--- casereader/casereader.py
+++ casereader/casereader.py
@@ -12,9 +12,8 @@ class CaseReader(commands.Cog):
     @commands.command()
     async def read(self, ctx, user: discord.User):
         membercases = await modlog.get_cases_for_member(ctx.guild, self.bot, member=user)
-        embed_requested = await ctx.embed_requested()
-        rendered_cases = [
-            await case.message_content(embed=embed_requested) for case in membercases
-        ]
+        rendered_cases = [await case.message_content(embed=False) for case in membercases]
         for message in rendered_cases:
-            await ctx.send(embed=message)
+            await ctx.send(
+                f"--------------------------------------------------------------------\n{message}"
+            )
