1503187a9dd1edcf4688cb4008b7e591f2671058
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-04-25 22:05:11 +0200

    the i'm dumb fix

--- check/check.py
+++ check/check.py
@@ -12,10 +12,10 @@ class Check(commands.Cog):
     @checks.mod()
     async def check(self, ctx, user: discord.User):
         ctx.assume_yes = True
+        member = False
         try:
             member = ctx.guild.get_member(user.id)
             await ctx.send(f":mag_right: Starting lookup for: {user.mention}({user.id})")
-            member = True
         except AttributeError:
             await ctx.send(
                 f":mag: This user is not in your guild anymore. Fetching reduced information for UID: {user.id}."
@@ -34,3 +34,5 @@ class Check(commands.Cog):
             await ctx.invoke(ctx.bot.get_command("listflag"), member=user)
         except:
             pass
+
+        await ctx.send("Lookup completed.")
