4a92dc5622fe4db64fb976c4e9826054fba80a4d
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-25 23:42:55 +0200

    prunecmdify i18n

--- prunecmd/prunecmd.py
+++ prunecmd/prunecmd.py
@@ -1,6 +1,10 @@
 from redbot.core import commands, checks
+from redbot.core.i18n import Translator, cog_i18n
+
+_ = Translator("Prunecmd", __file__)
 
 
+@cog_i18n(_)
 class Prunecmd(commands.Cog):
     """Prunecmd"""
 
@@ -14,4 +18,4 @@ class Prunecmd(commands.Cog):
         """Prunes the discord member list of your guild. This will kick all users without a role and 24h inactivity."""
 
         await ctx.guild.prune_members(days=1, compute_prune_count=False, reason="Member prune")
-        await ctx.send("The member list has been pruned successfully")
+        await ctx.send(_("The member list has been pruned successfully"))
