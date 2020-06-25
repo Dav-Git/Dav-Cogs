a24fea0c2ed3fb46bdb3a2d53dc0d6b8697abd50
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-25 23:04:35 +0200

    i18n exclusiveroles

--- exclusiveroles/exclusiveroles.py
+++ exclusiveroles/exclusiveroles.py
@@ -1,8 +1,12 @@
 import logging
 import discord
 from redbot.core import commands, checks, Config
+from redbot.core.i18n import Translator, cog_i18n
+
+_ = Translator("ExclusiveRoles", __file__)
 
 
+@cog_i18n(_)
 class ExclusiveRoles(commands.Cog):
     """Exclusive Roles"""
 
@@ -37,15 +41,15 @@ class ExclusiveRoles(commands.Cog):
         """Takes 2 Roles. Removes the second role if both roles are present on a user. """
 
         if not isinstance(role1, discord.Role) or not isinstance(role2, discord.Role):
-            return await ctx.send("You need to provide at least 2 roles")
+            return await ctx.send(_("You need to provide at least 2 roles"))
 
         else:
-            await ctx.send("\n`Started...`\n")
+            await ctx.send(_("\n`Started...`\n"))
             for user in ctx.guild.members:
                 if role1 in user.roles:
                     if role2 in user.roles:
                         await user.remove_roles(role2)
-            await ctx.send("\n`Completed.`\n")
+            await ctx.send(_("\n`Completed.`\n"))
 
     @commands.command()
     @checks.admin()
@@ -54,7 +58,7 @@ class ExclusiveRoles(commands.Cog):
 
         async with self.config.guild(ctx.guild).exclusives() as conf:
             conf.append((role1.id, role2.id))
-        await ctx.send("{} will now be overwritten by {}".format(role2.name, role1.name))
+        await ctx.send(_("{} will now be overwritten by {}").format(role2.name, role1.name))
 
     @commands.command()
     @checks.admin()
@@ -66,13 +70,13 @@ class ExclusiveRoles(commands.Cog):
                 try:
                     conf.remove((role1.id, role2.id))
                     await ctx.send(
-                        "{} will no longer be overwritten by {}".format(role2.name, role1.name)
+                        _("{} will no longer be overwritten by {}").format(role2.name, role1.name)
                     )
                 except:
-                    await ctx.send("```An Error occured```")
+                    await ctx.send(_("```An Error occured```"))
             else:
                 await ctx.send(
-                    "{} and {} are not registered as exclusive roles".format(
+                    _("{} and {} are not registered as exclusive roles").format(
                         role1.name, role2.name
                     )
                 )
@@ -86,17 +90,17 @@ class ExclusiveRoles(commands.Cog):
         embed = discord.Embed(title="Exclusivroles")
         text = ""
         if roles == []:
-            text = "No exclusive roles set"
+            text = _("No exclusive roles set")
         else:
             mentions = []
             for r in roles:
                 mentions.append(
-                    "\n{} overwrites {}".format(
+                    _("\n{} overwrites {}").format(
                         ctx.guild.get_role(r[0]).mention, ctx.guild.get_role(r[1]).mention,
                     )
                 )
             text = "\n".join(mentions)
-        embed.add_field(name="All exclusive role pairs:", value=text)
+        embed.add_field(name=_("All exclusive role pairs:"), value=text)
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -105,7 +109,7 @@ class ExclusiveRoles(commands.Cog):
         """Scans the entire user list for roles that have been set as exclusive."""
 
         async with ctx.channel.typing():
-            await ctx.send("``This may take a while...``")
+            await ctx.send(_("``This may take a while...``"))
             roles = await self.config.guild(ctx.guild).exclusives()
             for r in roles:
                 r_new = (ctx.guild.get_role(r[0]), ctx.guild.get_role(r[1]))
@@ -116,12 +120,14 @@ class ExclusiveRoles(commands.Cog):
                     roles.remove(r)
                     await self.config.guild(ctx.guild).exclusives.set(roles)
                     continue
-                await ctx.send("``Starting with {} and {}``".format(r_new[0].name, r_new[1].name))
+                await ctx.send(
+                    _("``Starting with {} and {}``").format(r_new[0].name, r_new[1].name)
+                )
                 for u in ctx.guild.members:
                     if r_new[0] in u.roles:
                         if r_new[1] in u.roles:
                             await u.remove_roles(
                                 r_new[1],
-                                reason="{} overwrites {}".format(r_new[0].name, r_new[1].name),
+                                reason=_("{} overwrites {}").format(r_new[0].name, r_new[1].name),
                             )
-        await ctx.send("``Retroscan completed.``")
+        await ctx.send(_("``Retroscan completed.``"))
