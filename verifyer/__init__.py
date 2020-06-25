1d05757e26f4340a2493047b8bcf512d442d6f12
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-25 17:06:15 +0200

    [verifyer] Release

---
+++ verifyer/__init__.py
@@ -0,0 +1,5 @@
+from .verifyer import Verifyer
+
+
+def setup(bot):
+    bot.add_cog(Verifyer())
---
+++ verifyer/verifyer.py
@@ -0,0 +1,81 @@
+from redbot.core import commands, Config
+import discord
+from typing import Optional
+
+
+class Verifyer(commands.Cog):
+    def __init__(self):
+        self.config = Config.get_conf(self, identifier=250620201622, force_registration=True)
+        default_guild = {
+            "text": "Welcome to [guild] please verify yourself to get access to the rest of the server by using ``[p]verify``.",
+            "verifiedtext": "",
+            "role": None,
+            "memrole": None,
+        }
+        self.config.register_guild(**default_guild)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        text = await self.config.guild(member.guild).text()
+        if text:
+            await member.send(text)
+        role = await self.config.guild(member.guild).role()
+        if role:
+            await member.add_roles(member.guild.get_role(role), reason="Verification required.")
+
+    @commands.command()
+    async def verify(self, ctx, member: Optional[discord.Member]):
+        if not member:
+            member = ctx.author
+        try:
+            verifiedtext = self.config.guild(ctx.guild).verifiedtext()
+            if verifiedtext:
+                await member.send(verifiedtext)
+        except discord.Forbidden:
+            pass
+        role = await self.config.guild(ctx.guild).role()
+        if role:
+            try:
+                await member.remove_roles(
+                    ctx.guild.get_role(role), reason="Member verified themselves."
+                )
+            except:
+                pass
+        memrole = await self.config.guild(ctx.guild).memrole()
+        if memrole:
+            await member.add_roles(
+                ctx.guild.get_role(memrole), reason="Member verified themselves."
+            )
+        try:
+            await ctx.message.remove()
+        except:
+            pass
+
+    @commands.group()
+    async def verifyerset(self, ctx):
+        """Settings for verifyer"""
+        pass
+
+    @verifyerset.command()
+    async def role(self, ctx, role: Optional[discord.Role]):
+        """Set the role to assign to a user on guild join.\n\nLeave empty to disable this feature."""
+        await self.config.guild(ctx.guild).role.set(role.id)
+        await ctx.send(f"Verification role set to {role.mention}.")
+
+    @verifyerset.command()
+    async def message(self, ctx, text: Optional[str]):
+        """Specify what message should be DMed to a user when they join the guild.\n\nLeave empty to disable this feature."""
+        await self.config.guild(ctx.guild).text.set(text)
+        await ctx.send(f"Message set to: ```{text}```")
+
+    @verifyerset.command()
+    async def verifiedmessage(self, ctx, text: Optional[str]):
+        """Specify what message should be DMed to a user when they verify themselves.\n\nLeave empty to disable this feature."""
+        await self.config.guild(ctx.guild).verifiedtext.set(text)
+        await ctx.send(f"Message set to: ```{text}```")
+
+    @verifyerset.command()
+    async def memberrole(self, ctx, role: Optional[discord.Role]):
+        """Set the role to assign to a user when they verify themselves.\n\nLeave empty to disable this feature."""
+        await self.config.guild(ctx.guild).memrole.set(role.id)
+        await ctx.send(f"Member role set to {role.mention}.")
