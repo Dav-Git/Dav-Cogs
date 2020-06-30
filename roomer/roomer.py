377da81e2953226d3170116dd72ffdb8495c2698
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-30 22:49:50 +0200

    [roomer] more logic fixes

--- roomer/roomer.py
+++ roomer/roomer.py
@@ -19,7 +19,7 @@ class Roomer(commands.Cog):
         settings = await self.config.guild(member.guild).all()
         if settings["auto"]:  # Autoroom stuff
             if not after.channel:
-                if not before.channel.members:
+                if len(before.channel.members) == 1:
                     await before.channel.delete(reason=_("Channel empty"))
                     if not member.guild.get_channel(settings["category"]).voice_channels:
                         await member.guild.create_voice_channel(
