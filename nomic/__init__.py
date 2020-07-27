f6773d5203743fb3bcbfb1a9c05ab5ba52eaa64a
Dav <dav@mail.stopdavabuse.de>
2020-07-28 00:20:17 +0200

    Delete __init__.py

--- nomic/__init__.py
+++
@@ -1,9 +0,0 @@
-from .nomic import NoMic
-
-__red_end_user_data_statement__ = "This cog does not store end user data."
-
-
-async def setup(bot):
-    cog = NoMic()
-    await cog.initialize()
-    bot.add_cog(cog)
