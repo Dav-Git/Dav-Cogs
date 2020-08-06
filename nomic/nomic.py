d483945f3b7283ad1624c2cff39ce37f9b91d36b
Trent Kable <trent@kablekompany.com>
2020-08-06 02:17:14 -0500

    [nicknamer]error message on double freeze (#56)
    * [nicknamer]error message on double freeze
    
    * black it
    
    * black it[2x for good measure ig]

--- nomic/nomic.py
+++ nomic/nomic.py
@@ -10,7 +10,7 @@ _ = Translator("nomic", __file__)
 class NoMic(commands.Cog):
     """#No-mic manager"""
 
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.config = Config.get_conf(self, identifier=889, force_registration=True)
         default = {"channels": {}}
         self.config.register_global(**default)
