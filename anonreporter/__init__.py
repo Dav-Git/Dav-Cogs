05a3b4d368c0c385f69eb36bdc65ea89959f5c30
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-08-17 14:02:37 +0200

    [Anonreporter] release

--- anonreporter/__init__.py
+++ anonreporter/__init__.py
@@ -7,4 +7,4 @@ with open(Path(__file__).parent / "info.json") as fp:
 
 
 def setup(bot):
-    bot.add_cog(AnonReporter())
+    bot.add_cog(AnonReporter(bot))
--- anonreporter/anonreporter.py
+++ anonreporter/anonreporter.py
@@ -2,6 +2,8 @@ from redbot.core import commands, checks, Config
 from redbot.core.i18n import cog_i18n, Translator
 
 import discord
+import asyncio
+from typing import Optional
 
 _ = Translator("AnonReporter", __file__)
 
@@ -11,10 +13,11 @@ class AnonReporter(commands.Cog):
     async def red_delete_data_for_user(self, *, user_id, requester):
         return  # This cog stores no EUD
 
-    def __init__(self):
+    def __init__(self, bot):
         self.config = Config.get_conf(self, identifier=171814082020, force_registration=True)
         default_guild = {"channel": None}
         default_global = {"rep_guild": None, "rep_channel": None}
+        self.bot = bot
         self.config.register_guild(**default_guild)
         self.config.register_global(**default_global)
 
@@ -43,3 +46,51 @@ class AnonReporter(commands.Cog):
                 channel=channel.mention, guild=ctx.guild.name
             )
         )
+
+    @commands.guild_only()
+    @commands.command()
+    async def anonreport(self, ctx, *, text: Optional[str]):
+        """Report something anonymously (don't include text to report via DM)"""
+
+        def msgcheck(m):
+            return m.guild is None and m.author.id == ctx.author.id
+
+        if not text:
+            if channel := await self.config.guild(ctx.guild).channel():
+                try:
+                    await ctx.author.send(_("Send your report here. You have 120s."))
+                    text = (
+                        await self.bot.wait_for("message", check=msgcheck, timeout=120)
+                    ).content
+                except discord.HTTPException:
+                    await ctx.send(
+                        _("Sending a DM failed. Make sure you allow DMs from the bot."),
+                        delete_after=15,
+                    )
+                except asyncio.TimeoutError:
+                    await ctx.author.send(_("Action timed out."))
+            else:
+                await ctx.send(_("Anonreport is not configured correctly."), delete_after=15)
+        else:
+            if channel := await self.config.guild(ctx.guild).channel():
+                await ctx.message.delete(delay=15)
+            else:
+                await ctx.send(_("Anonreport is not configured correctly."), delete_after=15)
+
+        if 0 < len(text) < 1000:
+            await ctx.guild.get_channel(channel).send(
+                _("**New anonymous report:**\n{report}").format(report=text)
+            )
+            await ctx.tick()
+        else:
+            await ctx.send(
+                _("Reports must be between 0 and 1000 characters long."), delete_after=15
+            )
+
+    @commands.command()
+    async def botreport(self, ctx, text: str):
+        """Report something to the bot owner anonymously."""
+        await self.bot.get_guild(await self.config.rep_guild()).get_channel(
+            await self.config.rep_channel()
+        ).send(_("**New anonymous report:**\n{report}").format(report=text))
+        await ctx.tick()
