6c66248bcca4de21bbd3e491713109fcffcb6032
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-10-25 02:23:01 +0100

    [prunecmd] add anoter safeguard due to recent events

--- prunecmd/prunecmd.py
+++ prunecmd/prunecmd.py
@@ -17,9 +17,15 @@ class Prunecmd(commands.Cog):
 
     @commands.command()
     @checks.admin()
-    async def prunecmd(self, ctx):
+    async def prunecmd(self, ctx, confirm: bool):
 
         """Prunes the discord member list of your guild. This will kick all users without a role and 24h inactivity."""
-
-        await ctx.guild.prune_members(days=1, compute_prune_count=False, reason="Member prune")
-        await ctx.send(_("The member list has been pruned successfully"))
+        if ctx.assume_yes or confirm:
+            await ctx.guild.prune_members(days=1, compute_prune_count=False, reason="Member prune")
+            await ctx.send(_("The member list has been pruned successfully"))
+        else:
+            await ctx.send(
+                _(
+                    "This will kick members from your guild. If you are sure you want to proceed run {command}"
+                ).format(command=f"``{ctx.clean_prefix}prunecmd yes``")
+            )
