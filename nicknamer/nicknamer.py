27f6ced4118f2834e54c82375e41c61f8db7e807
Trent Kable <trent@kablekompany.com>
2020-11-10 13:33:48 -0600

    adds decorator to validate bot permissions before proceeding freeze
    handles exception in listener

--- nicknamer/nicknamer.py
+++ nicknamer/nicknamer.py
@@ -87,7 +87,10 @@ class NickNamer(commands.Cog):
             for e in settings:
                 if after.id in e:
                     if after.nick != e[1]:
-                        await after.edit(nick=e[1], reason="Nickname frozen.")
+                        try:
+                            await after.edit(nick=e[1], reason="Nickname frozen.")
+                        except discord.errors.Forbidden:
+                            log.info(f"Missing permissions to change {before.nick} ({before.id}) in {before.guild.id}"
 
     @tasks.loop(minutes=10)
     async def _rename_tempnicknames(self):
@@ -116,6 +119,7 @@ class NickNamer(commands.Cog):
 
     @checks.mod()
     @commands.command()
+    @checks.bot_has_permissions(manage_nicknames=True)
     async def nick(self, ctx, user: discord.Member, *, reason: Optional[str]):
         """Forcibly change a user's nickname to a predefined string."""
         if not reason:
@@ -148,6 +152,7 @@ class NickNamer(commands.Cog):
 
     @checks.mod()
     @commands.command()
+    @checks.bot_has_permissions(manage_nicknames=True)
     async def cnick(self, ctx, user: discord.Member, nickname: str, *, reason: Optional[str]):
         """Forcibly change a user's nickname."""
         if not reason:
@@ -180,6 +185,7 @@ class NickNamer(commands.Cog):
 
     @checks.mod()
     @commands.command()
+    @checks.bot_has_permissions(manage_nicknames=True)
     async def freezenick(
         self,
         ctx,
@@ -242,6 +248,7 @@ class NickNamer(commands.Cog):
 
     @checks.mod()
     @commands.command()
+    @checks.bot_has_permissions(manage_nicknames=True)
     async def tempnick(
         self,
         ctx,
@@ -284,6 +291,7 @@ class NickNamer(commands.Cog):
 
     @checks.admin()
     @commands.group()
+    @checks.bot_has_permissions(manage_nicknames=True)
     async def nickset(self, ctx):
         """Nicknamer settings"""
         pass
