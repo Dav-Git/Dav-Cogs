43cb35c0a9401fb757d02a6d2dfbb12c18fc5590
Dav-Git <dav@mail.stopdavabuse.de>
2022-04-12 12:38:30 +0200

    [altmarker] add notify function

--- altmarker/altmarker.py
+++ altmarker/altmarker.py
@@ -31,7 +31,7 @@ class AltMarker(commands.Cog):
     Mark alt accounts
     """
 
-    __version__ = "0.1.0"
+    __version__ = "0.2.0"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         # Thanks Sinbad! And Trusty in whose cogs I found this.
@@ -50,35 +50,23 @@ class AltMarker(commands.Cog):
 
     @commands.Cog.listener()
     async def on_modlog_case_create(self, case: Case):
-        await case.guild.get_channel(469036337580998658).send("Listener triggered")
         member = (
             case.guild.get_member(case.user)
             if type(case.user) is int
             else case.guild.get_member(case.user.id)
         )
-        await case.guild.get_channel(469036337580998658).send(f"reeee: {member}")
-        try:
-            alts = await self.get_alts(member)
-        except Exception as e:
-            await case.guild.get_channel(469036337580998658).send(f"{e}")
-            return
-        await case.guild.get_channel(469036337580998658).send(f"alts: {alts}")
+        alts = await self.get_alts(member)
         if alts:
-            await case.guild.get_channel(469036337580998658).send("Alts found")
-
             channel_id = await self.config.guild(case.guild).notify()
             if channel_id:
-                await case.guild.get_channel(469036337580998658).send("Notify channel is set up")
-
                 channel = case.guild.get_channel(channel_id)
                 if channel:
-                    await case.guild.get_channel(469036337580998658).send("and exists")
-
-                    await channel.send(await self._get_alts_string(member))
-                    if case.channel:
-                        await case.guild.get_channel(469036337580998658).send(
-                            "The case even has a channel. Crazy shit."
+                    await channel.send(
+                        _("There are alt accounts for this member:\n{alt_message}").format(
+                            await self._get_alts_string(member)
                         )
+                    )
+                    if case.channel:
                         case_channel = (
                             case.guild.get_channel(case.channel)
                             if type(case.channel) is int
